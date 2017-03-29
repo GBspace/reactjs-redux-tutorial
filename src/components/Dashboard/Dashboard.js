3ed92762edeacb5f04f88bd9d781befb2d56913b
Tolik <talibasya@gmail.com>
2017-03-29 12:12:35 +0200

    implementing our dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,26 +4,17 @@ import './Dashboard.scss'
 export const Dashboard = (props) => (
   <div>
     <h2 className='dashboardContainer' >
-      Dashboard:
+      Dashboard visits:
       {' '}
       <span className='dashboard--green' >
         {props.dashboard}
       </span>
     </h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
   </div>
 )
 
 Dashboard.propTypes = {
-  dashboard   : React.PropTypes.number.isRequired,
-  doubleAsync : React.PropTypes.func.isRequired,
-  increment   : React.PropTypes.func.isRequired
+  dashboard   : React.PropTypes.number.isRequired
 }
 
 export default Dashboard
