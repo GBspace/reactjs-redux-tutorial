374ec4469cce67f06bbaf6965b54f65b2293dd53
Kamil Przeorski <kamil.przeorski@gmail.com>
2017-05-03 00:28:27 +0200

    prop types warning hotfix

--- src/components/Dashboard/ListJSX.js
+++ src/components/Dashboard/ListJSX.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import './Dashboard.scss'
 
 const ListJSX = ({ dashboardItems, onClick, activeIndex, onDragOver, onDragStart, onDrop }) => {
--- src/routes/Dashboard/components/Dashboard.js
+++ src/routes/Dashboard/components/Dashboard.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Dashboard from 'components/Dashboard'
 
 class DashboardRoute extends React.Component {
